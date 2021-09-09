05babb92ef19ff16db74d5d5958c804103dbb3b2
fisherlolipop <90281337+fisherlolipop@users.noreply.github.com>
2021-09-10 00:24:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
 const app = {
     data() {
         return {
